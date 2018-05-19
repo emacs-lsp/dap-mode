8415e1674d8c4219924349c3216a92b6c8092791
kyoncho <yyoncho@gmail.com>
2018-05-19 23:09:14 +0300

    Initial commit.

---
+++ dap-mode.el
@@ -0,0 +1,30 @@
+;;; dap-mode.el --- Debug Adapter Protocol mode      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Ivan
+
+;; Author: Ivan <kyoncho@myoncho>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Debug Adapter Protocol
+
+;;; Code:
+
+
+
+(provide 'dap-mode)
+;;; dap-mode.el ends here
