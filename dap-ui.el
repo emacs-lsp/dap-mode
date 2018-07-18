3707e98b05cf432d9fde70deda4e1d7693a31b28
ivan.yonchovski <ivan.yonchovski@tick42.com>
2018-07-18 10:10:33 +0300

    Handle no source location

--- dap-ui.el
+++ dap-ui.el
@@ -510,15 +510,16 @@ DEBUG-SESSION the new breakpoints for FILE-NAME."
 (defun dap-ui--stack-frame-changed (debug-session)
   "Handler for `dap-stack-frame-changed-hook'.
 DEBUG-SESSION is the debug session triggering the event."
-  (-when-let ((&hash "source" (&hash "path" path)
+  (-when-let ((&hash "source" source
                      "line" line
                      "column" column) (dap--debug-session-active-frame debug-session))
-    (goto-char (point-min))
-    (forward-line (1- line))
-    (forward-char column)
-    (dap-ui--set-debug-marker debug-session
-                         (lsp--uri-to-path path)
-                         (point))))
+    (when source
+      (goto-char (point-min))
+      (forward-line (1- line))
+      (forward-char column)
+      (dap-ui--set-debug-marker debug-session
+                           (lsp--uri-to-path (gethash "path" source))
+                           (point)))))
 
 (defun dap-ui--after-open ()
   "Handler for `lsp-after-open-hook'."
