4bdc795adc3e23deb0fbecd63e508677b4fe349c
Tom Regner <tom@goochesa.de>
2019-02-12 19:23:01 +0100

    Fix wrong `:type`
    `:type` must be `php` not `node`

--- dap-php.el
+++ dap-php.el
@@ -40,12 +40,12 @@
   (-> conf
       (dap--put-if-absent :dap-server-path dap-php-debug-program)
       (dap--put-if-absent :type "node")
-      (dap--put-if-absent :cwd projectile-project-root)
+      (dap--put-if-absent :cwd (lsp-find-session-folder (lsp-session) (buffer-file-name)))
       (dap--put-if-absent :name "Php Debug")))
 
 (dap-register-debug-provider "php" 'dap-php--populate-start-file-args)
 (dap-register-debug-template "Php Run Configuration"
-                             (list :type "node"
+                             (list :type "php"
                                    :cwd nil
                                    :request "launch"
                                    :name "Php Debug"
