ebb34c394067c81e29a36dc71452f326319a782d
Ivan Yonchovski <yyoncho@gmail.com>
2019-02-14 08:41:02 +0200

    Fixes https://github.com/yyoncho/dap-mode/issues/44

--- dap-mode.el
+++ dap-mode.el
@@ -31,8 +31,7 @@
 (require 'f)
 (require 'dash)
 (require 'dap-overlays)
-(eval-when-compile
-  (require 'cl))
+(require 'cl)
 
 (defconst dap--breakpoints-file (expand-file-name (locate-user-emacs-file ".dap-breakpoints"))
   "Name of the file in which the breakpoints will be persisted.")
