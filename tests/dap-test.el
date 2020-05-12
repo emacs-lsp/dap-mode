548a05ddaf891e05910b2c2c4777d3b3bb657a8d
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-05-11 22:36:49 -0700

    tests for both free functions and class methods

--- tests/dap-test.el
+++ tests/dap-test.el
@@ -58,7 +58,7 @@
 	  (should (equal (dap--parser-read p "Content-Length: 238\r\n\r\n{\"event\":\"output\",\"body\":{\"category\":\"stdout\",\"output\":\"2019-01-13 00:16:36  [ main ] - [ INFO com.inspur.common.utils.PropertiesBuilder.getConfig(32)]  从jar内加载:kafka-producer.properties\\n\",\"type\":\"output\"},\"seq\":9,\"type\":\"event\"}") '("{\"event\":\"output\",\"body\":{\"category\":\"stdout\",\"output\":\"2019-01-13 00:16:36  [ main ] - [ INFO com.inspur.common.utils.PropertiesBuilder.getConfig(32)]  从jar内加载:kafka-producer.properties\\n\",\"type\":\"output\"},\"seq\":9,\"type\":\"event\"}")))))
 
 
-(ert-deftest dap--python-test-1 ()
+(ert-deftest dap--python-test-free-function ()
   (let* ((document-symbols
 	  (list
 	   #s(lsp-symbol "dataclass" "Function" #s(location #s(point 0 0) #s(point 0 33)))
@@ -87,6 +87,37 @@
 
     (should (== actual expected))
     (should (== (dap-python--nearest-test expected) "::test_foo"))))
+
+(ert-deftest dap--python-test-class-method ()
+  (let* ((document-symbols
+	  (list
+	   #s(lsp-symbol "dataclass" "Function" #s(location #s(point 0 0) #s(point 0 33)))
+	   #s(lsp-symbol "Foo" "Class" #s(location #s(point 4 0) #s(point 6 0)))
+	   #s(lsp-symbol "value" "Variable" #s(location #s(point 5 4) #s(point 5 14)))
+	   #s(lsp-symbol "Bar" "Class" #s(location #s(point 9 0) #s(point 11 0)))
+	   #s(lsp-symbol "value" "Variable" #s(location #s(point 10 4) #s(point 10 14)))
+	   #s(lsp-symbol "TestClass" "Class" #s(location #s(point 13 0) #s(point 21 0)))
+	   #s(lsp-symbol "test_foo" "Function" #s(location #s(point 14 4) #s(point 17 0)))
+	   #s(lsp-symbol "foo" "Variable" #s(location #s(point 15 8) #s(point 15 24)))
+	   #s(lsp-symbol "test_bar" "Function" #s(location #s(point 18 4) #s(point 21 0)))
+	   #s(lsp-symbol "bar" "Variable" #s(location #s(point 19 8) #s(point 19 24)))))
+	 
+	 (cursor #s(point 15 4))
+
+	 (actual (dap-python--lsp-symbols-before-point cursor document-symbols))
+
+	 (expected
+	  (list 
+	   #s(lsp-symbol "dataclass" "Function" #s(location #s(point 0 0) #s(point 0 33)))
+	   #s(lsp-symbol "Foo" "Class" #s(location #s(point 4 0) #s(point 6 0)))
+	   #s(lsp-symbol "value" "Variable" #s(location #s(point 5 4) #s(point 5 14)))
+	   #s(lsp-symbol "Bar" "Class" #s(location #s(point 9 0) #s(point 11 0)))
+	   #s(lsp-symbol "value" "Variable" #s(location #s(point 10 4) #s(point 10 14)))
+	   #s(lsp-symbol "TestClass" "Class" #s(location #s(point 13 0) #s(point 21 0)))
+	   #s(lsp-symbol "test_foo" "Function" #s(location #s(point 14 4) #s(point 17 0))))))
+
+    (should (== actual expected))
+    (should (== (dap-python--nearest-test expected) "::TestClass::test_foo"))))
 
 
 (provide 'dap-test)
