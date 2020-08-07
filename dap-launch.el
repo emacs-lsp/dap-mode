ddfc18c8f4db3fa553245a233d94784a1540113e
Nikita Bloshchanevich <nikblos@outlook.com>
2020-08-07 10:06:16 +0200

    Remove Keywords, Author; fix comments referring to files

--- dap-launch.el
+++ dap-launch.el
@@ -1,9 +1,6 @@
-;; dap-mode-launch-json.el --- support launch.json -*- lexical-binding: t -*-
+;; dap-launch.el --- support launch.json -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2020 Nikita Bloshchanevich
-
-;; Author: Nikita Bloshchanevich <nikblos@outlook.com>
-;; Keywords: languages
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
