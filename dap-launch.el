982ac895521e93bf6e5c69023ebf5ed6264a8235
Nikita Bloshchanevich <nikblos@outlook.com>
2020-08-07 10:14:50 +0200

    Revert "Remove Keywords, Author; fix comments referring to files"
    This reverts commit ddfc18c8f4db3fa553245a233d94784a1540113e.
    
    It is unclear whether this module will be merged into dap-mode.el, so removing
    Author already is wrong.

--- dap-launch.el
+++ dap-launch.el
@@ -1,6 +1,9 @@
-;; dap-launch.el --- support launch.json -*- lexical-binding: t -*-
+;; dap-mode-launch-json.el --- support launch.json -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2020 Nikita Bloshchanevich
+
+;; Author: Nikita Bloshchanevich <nikblos@outlook.com>
+;; Keywords: languages
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
