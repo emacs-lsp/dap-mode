75ba92db135c0c24dacdf4e53c43eb391e86dd81
Ivan Andrus <iandrus@adobe.com>
2021-07-05 15:12:08 -0600

    Add dap-up-stack-frame and dap-down-stack-frame

--- dap-hydra.el
+++ dap-hydra.el
@@ -38,9 +38,11 @@
 _n_: Next           _ss_: Session            _bb_: Toggle          _dd_: Debug                 _ee_: Eval
 _i_: Step in        _st_: Thread             _bd_: Delete          _dr_: Debug recent          _er_: Eval region
 _o_: Step out       _sf_: Stack frame        _ba_: Add             _dl_: Debug last            _es_: Eval thing at point
-_c_: Continue       _sl_: List locals        _bc_: Set condition   _de_: Edit debug template   _ea_: Add expression.
-_r_: Restart frame  _sb_: List breakpoints   _bh_: Set hit count
-_Q_: Disconnect     _sS_: List sessions      _bl_: Set log message
+_c_: Continue       _su_: Up stack frame     _bc_: Set condition   _de_: Edit debug template   _ea_: Add expression.
+_r_: Restart frame  _sd_: Down stack frame   _bh_: Set hit count
+_Q_: Disconnect     _sl_: List locals        _bl_: Set log message
+                  _sb_: List breakpoints
+                  _sS_: List sessions
 "
   ("n" dap-next)
   ("i" dap-step-in)
@@ -50,6 +52,8 @@ _Q_: Disconnect     _sS_: List sessions      _bl_: Set log message
   ("ss" dap-switch-session)
   ("st" dap-switch-thread)
   ("sf" dap-switch-stack-frame)
+  ("su" dap-up-stack-frame)
+  ("sd" dap-down-stack-frame)
   ("sl" dap-ui-locals)
   ("sb" dap-ui-breakpoints)
   ("sS" dap-ui-sessions)
