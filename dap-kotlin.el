ed90a3d0887a9b956f0ddd56d4f0c73fefd1d920
Marie Katrine Ekeberg <5732795+themkat@users.noreply.github.com>
2022-05-13 16:34:50 +0200

    Debug attach configuration for dap-kotlin (#619)

--- dap-kotlin.el
+++ dap-kotlin.el
@@ -11,15 +11,30 @@
 	  (dap--put-if-absent :request "launch")
 	  (dap--put-if-absent :name "Kotlin Launch")))
 
+(defun dap-kotlin-populate-attach-args (conf)
+  (-> conf
+      (dap--put-if-absent :request "attach")
+      (dap--put-if-absent :name "Kotlin Attach")
+      (dap--put-if-absent :projectRoot (lsp-workspace-root))
+      (dap--put-if-absent :hostName "localhost")
+      (dap--put-if-absent :port 5005)
+      (dap--put-if-absent :timeout 2000)))
+
 (defun dap-kotlin-populate-default-args (conf)
   (setq conf (pcase (plist-get conf :request)
-			   ("launch" (dap-kotlin-populate-launch-args conf))
-			   (_ (error "Unsupported dap-request. Only launch is currently supported"))))
+               ("launch" (dap-kotlin-populate-launch-args conf))
+               ("attach" (dap-kotlin-populate-attach-args conf))
+               (_ (error "Unsupported dap-request"))))
   
   (-> conf
 	  (dap--put-if-absent :type "kotlin")
 	  (plist-put :dap-server-path (list lsp-kotlin-debug-adapter-path))))
 
 (dap-register-debug-provider "kotlin" #'dap-kotlin-populate-default-args)
+
+(dap-register-debug-template "Kotlin Attach"
+                             (list :type "kotlin"
+                                   :request "attach"
+                                   :noDebug nil))
 
 (provide 'dap-kotlin)
