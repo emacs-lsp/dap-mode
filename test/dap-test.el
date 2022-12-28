1af716b25396595e8bf44752fb71bb5fe07bdeaa
Frédéric Giquel <frederic.giquel@laposte.net>
2022-12-28 10:51:18 +0100

    Improve json-utils-sanitize-json (#701)

--- test/dap-test.el
+++ test/dap-test.el
@@ -172,6 +172,29 @@
 }"))
     (should (string= (dap-launch-test--sanitize-json orig) expected))))
 
+(ert-deftest dap-launch-test--delete-commas-before-comments ()
+  (let* ((orig "{
+    \"conf\": [
+        {
+          \"a\": \"b\",\t\v\u00A0
+        },
+        //{
+        //  \"b\": \"c\",\xD\u2028\u2029
+        //},\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006
+    ],\u2007\u2008\u2009\u200A\u202F\u205F\u3000
+}")
+         (expected "{
+    \"conf\": [
+        {
+          \"a\": \"b\"\t\v\u00A0
+        }
+        
+        
+        
+    ]\u2007\u2008\u2009\u200A\u202F\u205F\u3000
+}"))
+    (should (string= (dap-launch-test--sanitize-json orig) expected))))
+
 (ert-deftest dap-launch-test--comment-in-string ()
   (let ((orig "\"// orig\""))
     (should (string= orig (dap-launch-test--sanitize-json orig)))))
