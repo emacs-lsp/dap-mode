08db04945317e0857404b2342a42a4c4af220ea7
Steven Looman <steven.looman@gmail.com>
2023-02-23 11:57:36 +0100

    Bump version and finalize implementation

--- dap-magik.el
+++ dap-magik.el
@@ -26,8 +26,8 @@
 
 ;;; Code:
 
-(require 'lsp-magik)
 (require 'dap-mode)
+(require 'dap-utils)
 
 (defgroup dap-magik nil
   "DAP support for Magik."
@@ -36,57 +36,72 @@
   :tag "Dap Magik"
   :package-version '(dap-mode . "0.9.1"))
 
-(defcustom dap-magik-version "0.6.0"
+(defcustom dap-magik-version "0.7.1"
   "Version of DAP server."
-  :type `string
-  :group `dap-magik
+  :type 'string
+  :group 'dap-magik
   :package-version '(dap-mode . "0.9.1"))
 
-(defcustom dap-magik-download-url-da (format "https://github.com/StevenLooman/magik-tools/releases/download/%s/magik-debug-adapter-%s.jar" dap-magik-version dap-magik-version)
+(defcustom dap-magik-download-url (format "https://github.com/StevenLooman/magik-tools/releases/download/%s/magik-debug-adapter-%s.jar" dap-magik-version dap-magik-version)
   "URL of DAP server to download."
-  :type `string
-  :group `dap-magik
+  :type 'string
+  :group 'dap-magik
   :package-version '(dap-mode . "0.9.1"))
 
-(lsp-dependency
- 'magik-da
- `(:download :url dap-magik-download-url-da
-             :store-path ,(f-join lsp-server-install-dir "magik-lsp" (format "magik-debug-adapter-%s.jar" dap-magik-version))))
-
-(defcustom dap-magik-da-path
-  (f-join lsp-server-install-dir (format "magik-lsp/magik-debug-adapter-%s.jar" dap-magik-version))
+(defcustom dap-magik-path (f-join dap-utils-extension-path (format "magik/magik-debug-adapter-%s.jar" dap-magik-version))
   "Path of the debug adapter."
   :type 'string
-  :group `dap-magik
-  :package-version '(lsp-mode . "8.0.1"))
+  :group 'dap-magik
+  :package-version '(dap . "0.9.1"))
+
+(defcustom dap-magik-java-path (cond ((eq system-type 'windows-nt) "$JAVA_HOME/bin/java")
+                                     (t "java"))
+  "Path of the java executable."
+  :type 'string
+  :group 'dap-magik
+  :package-version '(dap-mode . "0.9.1"))
 
 (defcustom dap-magik-attach-host "127.0.0.1"
   "Host to connect to when attaching to a session."
   :type 'string
-  :group `dap-magik
+  :group 'dap-magik
   :package-version '(dap-mode . "0.9.1"))
 
-(defcustom dap-magik-attach-port 21821
-  "Host to port to when attaching to a session."
+(defcustom dap-magik-attach-port 32000
+  "Port to connect to when attaching to a session."
   :type 'integer
-  :group `dap-magik
+  :group 'dap-magik
   :package-version '(dap-mode . "0.9.1"))
 
+(defcustom dap-magik-path-mapping []
+  "Path mapping to apply."
+  :type 'vector
+  :group 'dap-magik
+  :package-version '(dap-mode . "0.9.1"))
 
+(defun dap-magik-download-da ()
+  "Download the Magik Debug Adapter."
+  (unless (file-exists-p (f-join dap-utils-extension-path "magik"))
+    (make-directory (f-join dap-utils-extension-path "magik") t))
+  (unless (file-exists-p dap-magik-path)
+    (url-copy-file dap-magik-download-url dap-magik-path 'overwrite)
+    (message "%s: Downloading done!" "dap-magik")))
+
+(dap-magik-download-da)
 
 (defun dap-magik--populate-attach-args (conf)
   "Populate CONF with the required arguments."
   (-> conf
-      (dap--put-if-absent :da-server-path (list lsp-magik-java-path "-jar" dap-magik-da-path "--debug"))
+      (dap--put-if-absent :dap-server-path (list dap-magik-java-path "-jar" dap-magik-path "--debug"))
       (dap--put-if-absent :type "magik")
       (dap--put-if-absent :request "attach")
       (dap--put-if-absent :connect (list :host dap-magik-attach-host
 					 :port dap-magik-attach-port))))
 
-(dap-register-debug-template "Magik: Attach"
+(dap-register-debug-template "Magik Attach Configuration"
                              (list :type "magik"
                                    :request "attach"
-				   :name "Magik: Attach"))
+				   :name "Magik::Attach"))
 
 (dap-register-debug-provider "magik" #'dap-magik--populate-attach-args)
 
